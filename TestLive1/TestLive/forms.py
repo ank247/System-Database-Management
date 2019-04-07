1da290ec6cbc560bdbd0effdd3c3fde4fac88288
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-04-07 14:49:54 +0530

    Update forms.py

--- TestLive1/TestLive/forms.py
+++ TestLive1/TestLive/forms.py
@@ -11,7 +11,7 @@ class ContactForm(forms.Form):
     message=forms.CharField(widget=forms.Textarea,max_length=300)
     sender=forms.EmailField()
     cc_myself=forms.BooleanField(required=False)
-    bcc=forms.BooleanField(required=False)
+    bcc=forms.EmailField()
     
 class fileUpload(forms.Form):
     title = forms.CharField(max_length = 30)
