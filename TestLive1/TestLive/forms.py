7e5802d24f056483466d2690ed2f86d7409171d5
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-04-07 09:07:35 +0530

    Update forms.py

--- TestLive1/TestLive/forms.py
+++ TestLive1/TestLive/forms.py
@@ -1,7 +1,17 @@
 
 from django import forms
 from django.db import models
+from .models import SignUp, Login
 
+class NameForm(forms.Form):
+    
+    
+class ContactForm(forms.Form):
+    sender=forms.EmailField(max_length=50)
+    text=forms.CharField(required=textarea,max_length=300)
+    cc_sender=forms.EmailField(max_length=50)
+    receipients=forms.EmailField(max_length=50)
+    
 class fileUpload(forms.Form):
     title = forms.CharField(max_length = 30)
     file = forms.FileField()
