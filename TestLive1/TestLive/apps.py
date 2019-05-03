4b39fbd3009aad68c23d2b472e80e19de99fa80b
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-05-03 20:01:19 +0000

    Add files via upload

--- TestLive1/TestLive/apps.py
+++ TestLive1/TestLive/apps.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
+
 class TestliveConfig(AppConfig):
     name = 'TestLive'
+
+    def ready(self):
+        import users.signals
 
