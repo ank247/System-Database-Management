05ba86556d10210d41397cc36c75852151266a82
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-05-12 03:34:03 +0000

    Update views.py

--- TestLive1/TestLive/views.py
+++ TestLive1/TestLive/views.py
@@ -1,11 +1,121 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from .forms import fileUpload, NameForm
-from django.core.mail import send_email, BadHeaderError
-from django.http import HttpResponseRedirect, HttpResponse
+
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect,StreamingHttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm,EmailForm,ProfileUpdateForm,UserUpdateForm
+from .models import Profile
+from django.core.mail import BadHeaderError,send_mail
+import csv
+from django.template import Context,loader
 from .models import Exam, Login, Set, Analytic, User, Registration, Plan, E_Commerce, Payment, Choose_Plan, Query, Contact_Us, Upload_Form 
+from .forms import fileUpload, NameForm
+
+
+def register(request):
+    if request.method=='POST':
+       form=UserRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+          username=form.cleaned_data.get("username")
+          return HttpResponseRedirect('')
+    else:
+        form=UserRegistrationForm()
+    return render(request,'TestLive/register.html',{'form':form})
+
+
+
+@login_required
+def profile(request):
+    try:
+        profile=request.user.profile
+    except Profile.DoesNotExist:
+        profile=Profile(user=request.user)
+    if request.method=='POST':
+       u_form=UserUpdateForm(request.POST,instance=request.user)
+       p_form=ProfileUpdateForm(request.POST or None,request.FILES,instance=profile)
+       if u_form.is_valid() and p_form.is_valid():
+          p_form.save()
+          u_form.save()
+          return HttpResponseRedirect('')
+    else:
+        u_form=UserUpdateForm(instance=request.user)
+        p_form=ProfileUpdateForm(instance=request.user.profile)
+
+    context={
+             'u_form':u_form,
+             'p_form':p_form
+            }
+    return render(request,'TestLive/profile.html',context) 
+
+
+def feeds(request):
+    return render(request,'TestLive/feeds.html')
+
+"""
+def send_email(request):
+    if request.method=='POST':
+       email_form=EmailForm(request.POST)
+       if email_form.is_valid():
+          email_form.save()
+          send_mail('subject','message','email_form.from_email',['to_email@gmail.com'],fail_silently=False)
+          return HttpResponse('Hello')  
+    else:
+        #email_form=EmailForm()
+        return HttpResponse("Enter Valid Form")
+    return redirect(request,'TestLive/email.html',{'email':email_form})
+"""    
+
+
+
+def send_email(request):
+    if request.method=='POST':
+       subject = request.POST.get(request.POST,'subject', '')
+       message = request.POST.get('message', '')
+       from_email = 'TestLive@ac.in'
+       if subject and message and from_email:
+          try:
+              send_mail(subject, message, from_email, ['admin@example.com'])
+          except BadHeaderError:
+              return HttpResponse('Invalid header found.')
+          return HttpResponseRedirect('')
+    else:
+        return HttpResponse('Make sure all fields are entered and valid.')
+    return redirect(request,'TestLive/email.html')
+
+
+
+class Echo():
+      def write(self,value):
+          return value
+
+
+def csv_view(request):
+    """
+    response=HttpResponse(content_type='text/csv')
+    response['Content-Disposition']='attachment;filename="Charan_PG.xlsx"'
+    csv_data=(
+        ('First row','Foo','Bar','Baz'),
+        ('Second row','A','B','Testing'),
+    )
+    
+    t=loader.get_template('csv_to_txt.txt')
+    c=Context({
+      'data':csv_data,
+    })
+    response.write(t.render(c))
+    return response
+
+    """
+    rows=(["Row {}".format(idx),str(idx)] for idx in range(65536))
+    pseudo_buffer=Echo()
+    writer=csv.writer(pseudo_buffer)
+    response=StreamingHttpResponse((writer.writerow(row) for row in rows),content_type="text/csv")
+    response['Content-Disposition']='attachment;filename="Charan_PG.xlsx"'
+    return response
+
 
 def sign_up(request):
     if request.method == "POST":
