ee8eb8e853fc2b3e7d291616f421a68281d617cf
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-05-16 12:59:34 +0000

    Update urls.py

--- TestLive1/TestLive1/urls.py
+++ TestLive1/TestLive1/urls.py
@@ -14,12 +14,53 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url, include
+from django.conf.urls import url,include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-#from TestLive.views import detail
+from django import views
+from django.contrib.auth import views as auth_view
+from TestLive import views as TestLive_view
+from TestLive.syndicate import LatestEntries
+from blogs import views as blog_view,urls as blog_url
+from payment import views as payment_view
+from exam_papers import views as examPaper_view
+from emails import views as email_view
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^TestLive/', include('TestLive.urls')),
+    url('login/',auth_view.LoginView.as_view(template_name='TestLive/login.html'),name='login'),
+    url('logout/',auth_view.LogoutView.as_view(template_name='TestLive/logout.html'),name='logout'),
+    url('feeds/',LatestEntries(),name='feeds'),
+    url('',include(blog_url),name='blog'),
 ]
+
+
+
+urlpatterns+=[
+   url('register/',TestLive_view.register,name='register'),
+   url('profile/',TestLive_view.profile,name='profile'),
+   url('csv_view/',TestLive_view.csv_view,name='csv_view'),
+]
+
+
+
+urlpatterns+=[
+   url('exam_paper/',examPaper_view.paper,name='paper'),
+   url('email/',email_view.send_email,name='emails'),
+   url('payment/',payment_view.payment,name='payment'),
+]
+
+
+
+urlpatterns+=[
+   url('',blog_view.update_blog,name='blog'),
+]
+
+
+
+if settings.DEBUG:
+   urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
