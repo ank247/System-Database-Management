16044c5ce3f281344bb4bbfde58f6dc768f21a6c
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-05-16 12:55:53 +0000

    Update forms.py

--- TestLive1/blogs/forms.py
+++ TestLive1/blogs/forms.py
@@ -1,13 +1,12 @@
 
+from django.forms import ModelForm
+from .models import blogModel
 from django import forms
-#from django.contrib.auth.forms import UserCreationForm
-#from django.contrib.auth.models import User
-from .models import BlogModel
- 
-class BlogForm(forms.Form):
-      blog=forms.CharField(max_length=500)            
-      class Meta:
-            model=BlogModel
-            tables=['user','blog']
-            
+
+
+class blogForm(ModelForm):
+    email=forms.EmailField()
+    class Meta:
+        model=blogModel
+        fields=['email','title','blog']
 
