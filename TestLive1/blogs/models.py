b1030f085143ac7220be462a0e72f670962ee827
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-05-16 12:54:55 +0000

    Update models.py

--- TestLive1/blogs/models.py
+++ TestLive1/blogs/models.py
@@ -3,12 +3,33 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class BlogModel(models.Model):
-      user=models.ForeignKey(User,on_delete=models.CASCADE)
-      blog=models.CharField(max_length=250)
-    
+
+class blogModel(models.Model):
+      user=models.TextField(max_length=150)
+      date=models.DateTimeField()
+      title=models.CharField(max_length=150)
+      blog=models.CharField(max_length=1000)
+      
+      class Meta:
+          ordering=["user"]
+
       def __str__(self):
-          return self.user.username
+          return self.title
 
+
+"""
+@receiver(post_save,sender=User)
+def create_user_blog(sender,instance,created,**kwargs):
+    if created:
+       blogModel.objects.create(title=instance)
+
+
+@receiver(post_save,sender=User)
+def save_user_blog(sender,instance,**kwargs):
+    instance.blogmodel.save()
+
+"""
