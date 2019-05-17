b10844a6df93eddf21a25ee6a474af9ef2ce3b68
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-05-17 05:47:23 +0000

    Update views.py

--- TestLive1/blogs/views.py
+++ TestLive1/blogs/views.py
@@ -5,12 +5,15 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
+from django.core.paginator import Paginator
 from .forms import blogForm
 from .models import blogModel
 
 
+
 class blogList(ListView):
     model=blogModel
+
 
 
 @login_required
@@ -20,8 +23,18 @@ def update_blog(request):
        if blog.is_valid():
           blog.save()
           #user=form.cleaned_data.get('username')
-          return HttpResponseRedirect('/blogs/blog/')
+          return HttpResponseRedirect('/blogs/')
     else:
        blog=blogForm(instance=request.user)
     return render(request,'blogs/blogs.html',{'blog':blog})
+
+
+
+def list_blog(request):
+    blog=blogModel.objects.all()
+    p=Paginator(blog,1)
+    page=request.GET.get('page',1)
+    p1=p.page(page)
+    return render(request,'blogs/blogList.html',{'page':p1})    
+    
 
