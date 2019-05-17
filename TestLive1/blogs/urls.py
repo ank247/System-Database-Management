8baeb07205205d4dff3a66ede76472534d488c6b
ANKIT KUMAR <ankit247shbm@gmail.com>
2019-05-17 05:48:55 +0000

    Update urls.py

--- TestLive1/blogs/urls.py
+++ TestLive1/blogs/urls.py
@@ -1,8 +1,12 @@
 
 from django.conf.urls import url
 from .views import blogList
+from . import views
 
 
 urlpatterns=[
-   url('',blogList.as_view(template_name='blogs/blogList.html'),name='blogList'),
+   #url('',blogList.as_view(template_name='blogs/blogList.html'),name='blogList'),
+   url('',views.list_blog,name='list_blog'),
 ]
+
+
